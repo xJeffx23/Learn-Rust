5b9d2598ee7a7b1f14a1ae9beea12d32be093503
Jefferson <xjeffcmx23@gmail.com>
2025-05-30 22:32:22 -0600

    Day 2

--- curso/src/main.rs
+++ curso/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
+    // Variables
     let age: i32 = 20 ;
     let name: &str = "Jefferson";
+    // Temperatures
+    let  temp_max: i8 = 26;
+    let  temp_min: i8 = 17;
 
     
     println!("Hi! My name is {} and I am {} years",name, age);
-
+    println!("The maximum temperature today is {}°C and the minimum is {}°C", temp_max, temp_min);
 }
